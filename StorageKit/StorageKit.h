89c61aab5e4f8addf0415644a8ce9d0f2d865f08
Mladen <excellence.cro@gmail.com>
2020-06-14 19:06:32 +0200

    Initial Commit

---
+++ StorageKit/StorageKit.h
@@ -0,0 +1,19 @@
+//
+//  StorageKit.h
+//  StorageKit
+//
+//  Created by Mladen Mikic on 14/06/2020.
+//  Copyright © 2020 Mladen Mikic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StorageKit.
+FOUNDATION_EXPORT double StorageKitVersionNumber;
+
+//! Project version string for StorageKit.
+FOUNDATION_EXPORT const unsigned char StorageKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StorageKit/PublicHeader.h>
+
+
